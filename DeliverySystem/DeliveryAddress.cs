2072b4f1433ccd2688c73dfcfc6200e2f622afe5
Zeinab-BS <zm307692@gmail.com>
2026-08-14 18:05:14 +0300

    DeliveryAddress Struct

---
+++ DeliverySystem/DeliveryAddress.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASS1_OOP
+{
+    internal struct DeliveryAddress
+    {
+        public string City;
+        public string Street;
+        public int BuildingNumber;
+
+        public DeliveryAddress(string _city, string _street, int _bNum)
+
+        {
+
+            City = _city;
+            Street = _street;
+            BuildingNumber = _bNum;
+
+
+
+        }
+
+        public string GetFullAddress()
+        {
+            return $"Destination:{BuildingNumber}{Street},{City}";
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ DeliverySystem/DeliveryCenter.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASS1_OOP
+{
+    internal class DeliveryCenter
+    {
+
+        private Shipment[] shipment;
+
+        public DeliveryCenter()
+        {
+            shipment = new Shipment[10];
+
+        }
+
+        #region indexres
+        public Shipment this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < shipment.Length)
+                {
+                    return shipment[index];
+
+                }
+
+                return null;
+
+            }
+
+            set
+            {
+                if (index >= 0 && index < shipment.Length)
+                {
+                    shipment[index] = value;
+
+                }
+
+
+            }
+
+        }
+
+        public Shipment this[string trackingCode]
+        {
+            get
+            {
+                for (int i = 0; i < shipment.Length; i++)
+                {
+                    if (shipment[i] != null && shipment[i].TrackingCode == trackingCode)
+                    {
+                        return shipment[i];
+                    }
+
+                }
+                return null;
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+        #endregion
+
+
+
+        #region AddShipment
+
+
+        public bool AddShipment(Shipment s)
+        {
+            for (int i = 0; i < shipment.Length; i++)
+            {
+                if (shipment[i] == null)
+                {
+                    shipment[i] = s;
+                    return true;
+                }
+
+            }
+
+            return false;
+        }
+
+
+
+
+
+
+        #endregion
+
+
+
+
+    }
+}
